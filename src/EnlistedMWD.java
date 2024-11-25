f5a0870eed34d70528ec247f7c3e05936a58f65f
modularmania <mcand707@yahoo.com>
2024-11-25 14:22:42 -0500

    Initial Commit.

---
+++ src/EnlistedMWD.java
@@ -0,0 +1,85 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "enlisted_mwd")
+public class EnlistedMWD {
+
+    @Column(name = "uid", nullable = true, length = 10)
+    private String uid;
+
+    @Column(name = "branch", nullable = true, length = 255)
+    private String branch;
+
+    @Column(name = "rank", nullable = true, length = 255)
+    private String rank;
+
+    @Column(name = "first_name", nullable = true, length = 255)
+    private String firstName;
+
+    @Column(name = "middle_name", nullable = true, length = 255)
+    private String middleName;
+
+    @Column(name = "last_name", nullable = true, length = 255)
+    private String lastName;
+
+    @Column(name = "paired", nullable = true)
+    private boolean paired;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getBranch() {
+        return branch;
+    }
+
+    public void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isPaired() {
+        return paired;
+    }
+
+    public void setPaired(boolean paired) {
+        this.paired = paired;
+    }
+}
